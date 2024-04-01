cdeeeec7ee6eea82f0b8aed0dbfaa32d37b68b42
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-04-01 16:23:50 -0700

    Update solver.js

--- submission/solver.js
+++ submission/solver.js
@@ -10,11 +10,38 @@ parts = balanced_equation.split(" -> ")[0].split(" + ").concat(balanced_equation
 result_obj = {}
 parts.forEach((part)=>{
 
-    result_obj[part[0]] = part.slice(1)
+    result_obj[part.slice(1)] = part[0]
 })
+console.log(result_obj)
 
 
+firstMole = new Mole(getParam("stoichFromMolecule"))
+dtype = getParam("stoichFromUnits")
+if(dtype == "gas"){
+    firstMole.from_stp(getParam("stoichFromAmt"))
+}else if(dtype == "particles"){
+    firstMole.from_particles(getParam("stoichFromAmt"))
+}else if(dtype == "mass"){
+    firstMole.from_mass(getParam("stoichFromAmt"))
+}else{
+    firstMole.amt = getParam("stoichFromAmt")
+}
 
-first = getParam()
+
+secondMole = new Mole(getParam("stoichToMolecule"))
+firstMole.to_other_mole(secondMole, result_obj[getParam("stoichFromMolecule")], result_obj[getParam("stoichFromMolecule")])
+solved_result = ""
+out_dtype = getParam("stoichToUnits")
+if(out_dtype == "gas"){
+    solved_result = secondMole.to_stp(getParam("stoichFromAmt"))
+}else if(out_dtype == "particles"){
+    solved_result=secondMole.to_particles(getParam("stoichFromAmt"))
+}else if(out_dtype == "mass"){
+    solved_result=secondMole.to_mass(getParam("stoichFromAmt"))
+}else{
+    solved_result = secondMole.amt + " mol"
+}
+
+document.getElementById("math").innerText = `$${secondMole.equ_prefix}=${solved_result}$`
 
 //alert(balanced_equation+"   "+parts)
