c1ae47cd72f0dccdd755e691765169537489aa56
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-04-08 18:20:54 -0700

    Fix sig figs calculation

--- submission/sigfigs.js
+++ submission/sigfigs.js
@@ -1,15 +1,15 @@
-//https://stackoverflow.com/questions/22884720/what-is-the-fastest-way-to-count-the-number-of-significant-digits-of-a-number
-
-var log10 = Math.log(10);
 function getSignificantDigitCount(n) {
-    n = Math.abs(String(n).replace(".", "")); //remove decimal and make positive
-    if (n == 0) return 0;
-    while (n != 0 && n % 10 == 0) n /= 10; //kill the 0s at the end of n
-
-    return Math.floor(Math.log(n) / log10) + 1; //get number of digits
+  n = n.replace(".", "");
+  const num_nonzero_matches = n.match(/[1-9]/g);
+  const num_nonzero = num_nonzero_matches ? num_nonzero_matches.length : 0;
+  //https://regexr.com/7ul19
+  const pattern = /(?<=[1-9])0*$/;
+  let match = pattern.exec(n);
+  const num_significant_zeros = match ? match[0].length : 0;
+  return num_nonzero + num_significant_zeros;
 }
 
-function matchPrecision(inNum, outNum){
-    outNum = Number(outNum)
-    return outNum.toPrecision(getSignificantDigitCount(inNum))
+function matchPrecision(inNum, outNum) {
+  outNum = Number(outNum);
+  return outNum.toPrecision(getSignificantDigitCount(inNum));
 }
